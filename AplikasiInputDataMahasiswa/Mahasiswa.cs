c0d87c5bfe6f281e378bd9fcef5b6c032225f828
annisasilvia04 <122153114+annisasilvia04@users.noreply.github.com>
2023-09-25 21:51:04 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string Nim;
+        public string Nama;
+        public string Kelas;
+        public char Nilai_Huruf;
+        public int Nilai;
+
+    }
+}
